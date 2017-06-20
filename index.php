01e99cd04dc0c8456412f42c64e6170f70fd347a
MarcoVanderEecken <vandereeckenmarco@gmail.com>
2017-06-20 16:30:05 +0200

    Uploading Folder structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Temporary
+ * Date: 6/20/2017
+ * Time: 4:27 PM
+ */
+    echo "hello, main index page started load";
+
+    include('php/test.php');
---
+++ php/test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Temporary
+ * Date: 6/20/2017
+ * Time: 4:22 PM
+ */
+ echo "<br> include test one";
